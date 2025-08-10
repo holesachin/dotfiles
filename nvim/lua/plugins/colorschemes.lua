35906120e64f3893dd73df543cf6b0c8cbdbf876
holesachin <holesachin007@gmail.com>
2025-08-10 22:18:42 +0530

    Sun Aug 10 10:18:42 PM IST 2025

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -1,25 +1,28 @@
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('onedark').setup {
-      style = 'darker',
-      transparent = true,
-      term_colors = true,
-
-      barbecue = {
+  {
+    "navarasu/onedark.nvim",
+    enabled = true,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker',
         transparent = true,
-      },
+        term_colors = true,
 
-      -- Plugins Config --
-      diagnostics = {
-        darker = true, -- darker colors for diagnostic
-        undercurl = true,   -- use undercurl instead of underline for diagnostics
-        background = true,    -- use background color for virtual text
-      },
+        barbecue = {
+          transparent = true,
+        },
 
-    }
-    -- Enable theme
-    require('onedark').load()
-  end
+        -- Plugins Config --
+        diagnostics = {
+          darker = true, -- darker colors for diagnostic
+          undercurl = true,   -- use undercurl instead of underline for diagnostics
+          background = true,    -- use background color for virtual text
+        },
+
+      }
+      -- Enable theme
+      require('onedark').load()
+    end
+  },
 }
