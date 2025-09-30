14c54002f82bd4e636bfdff578307ba47d672ecb
holesachin <holesachin007@gmail.com>
2025-09-30 22:26:30 +0530

    dwm:default

---
+++ dwm/themes/oneish.h
@@ -0,0 +1,42 @@
+static char c000000[]                    = "#000000"; // placeholder value
+
+static char normfgcolor[]                = "#abb2bf";
+static char normbgcolor[]                = "#282c34";
+static char normbordercolor[]            = "#444444";
+static char normfloatcolor[]             = "#db8fd9";
+
+static char selfgcolor[]                 = "#e06c75";
+static char selbgcolor[]                 = "#282c34";
+static char selbordercolor[]             = "#e5c07b";
+static char selfloatcolor[]              = "#56b6c2";
+
+static char titlenormfgcolor[]           = "#abb2bf";
+static char titlenormbgcolor[]           = "#282c34";
+static char titlenormbordercolor[]       = "#444444";
+static char titlenormfloatcolor[]        = "#db8fd9";
+
+static char titleselfgcolor[]            = "#e06c75";
+static char titleselbgcolor[]            = "#282c34";
+static char titleselbordercolor[]        = "#005577";
+static char titleselfloatcolor[]         = "#005577";
+
+static char tagsnormfgcolor[]            = "#abb2bf";
+static char tagsnormbgcolor[]            = "#282c34";
+static char tagsnormbordercolor[]        = "#444444";
+static char tagsnormfloatcolor[]         = "#db8fd9";
+
+static char tagsselfgcolor[]             = "#e06c75";
+static char tagsselbgcolor[]             = "#282c34";
+static char tagsselbordercolor[]         = "#005577";
+static char tagsselfloatcolor[]          = "#005577";
+
+static char hidnormfgcolor[]             = "#005577";
+static char hidselfgcolor[]              = "#227799";
+static char hidnormbgcolor[]             = "#222222";
+static char hidselbgcolor[]              = "#222222";
+
+static char urgfgcolor[]                 = "#bbbbbb";
+static char urgbgcolor[]                 = "#222222";
+static char urgbordercolor[]             = "#ff0000";
+static char urgfloatcolor[]              = "#db8fd9";
+
---
+++ dwm/themes/red.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#2a2a2a";
+static const char col_gray2[]       = "#ede6e6";
+static const char col_gray3[]       = "#ede6e6";
+static const char col_gray4[]       = "#ede6e6";
+static const char col_gray5[]       = "#c71212";
+static const char col_cyan[]        = "#c71212";
