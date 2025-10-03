19e4b0f90cb5ad5986cc245eaab9bb26331788cf
holesachin <holesachin007@gmail.com>
2025-10-03 18:16:02 +0530

    before status text fix

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -15,14 +15,14 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 3;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 14;        /* vertical padding for statusbar */
+static const int vertpadbar         = 14;       /* vertical padding for statusbar */
 static const char *fonts[]          = { "JetBrains Mono:style:medium:size=9" ,"ShureTechMono Nerd Font Mono:style:medium:size=18" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char *colors[][3]      = {
 	/*                   fg           bg         border   */
 	[SchemeNorm]     = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]      = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeStatus]   = { col_gray3, col_gray1, col_gray1 }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeStatus]   = { col_gray3, "#000000", col_cyan  }, // Statusbar right {text,background,not used but cannot be empty}
 	[SchemeTagsSel]  = { col_cyan,  col_gray1, col_gray1 }, // Tagbar left selected {text,background,not used but cannot be empty}
 	[SchemeTagsNorm] = { col_gray3, col_gray1, col_gray1 }, // Tagbar left unselected {text,background,not used but cannot be empty}
 	[SchemeInfoSel]  = { col_gray3, col_gray1, col_gray1 }, // infobar middle  selected {text,background,not used but cannot be empty}
@@ -79,7 +79,8 @@ static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,       spawn,         SHCMD("~/.config/rofi/scripts/launcher_t7") },
+	{ MODKEY,                       XK_d,      spawn,          SHCMD("~/.config/rofi/scripts/launcher_t7") },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          SHCMD("zen-beta") },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
